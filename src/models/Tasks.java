477f38881083e9f0a6f0e54f50247f88e3021b99
seiji.nakatani <silvercat610@icloud.com>
2020-04-28 08:45:27 +0900

    Add getAllMessages

--- src/models/Tasks.java
+++ src/models/Tasks.java
@@ -7,9 +7,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllTasks",
+            query = "SELECT m FROM Tasks AS m ORDER BY m.id DESC"
+            )
+})
 @Table(name = "tasks")
 
 public class Tasks {
@@ -31,7 +39,7 @@ public class Tasks {
         return id;
     }
 
-    public void setId(Integer Id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
